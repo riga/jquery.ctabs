23e4b75888237f410d661d7a5326e8acfead931d
Riga <marcelrieger@me.com>
2013-03-22 17:51:00 +0100

    Add minified verison.

---
+++ jquery.tabs.min.js
@@ -0,0 +1,10 @@
+/*!
+ * jQuery Tabs Plugin v0.1
+ * https://github.com/riga/jquery.tabs
+ *
+ * Copyright 2012, Marcel Rieger
+ * Dual licensed under the MIT or GPL Version 3 licenses.
+ * http://www.opensource.org/licenses/mit-license
+ * http://www.opensource.org/licenses/GPL-3.0
+ *
+ */
