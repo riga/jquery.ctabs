a1756a4a3cd9c5bed7c0e1eb509e09c04bd3d76e
Riga <marcelrieger@me.com>
2013-03-24 14:02:36 +0100

    Add css file, add processing draft.

--- jquery.ctabs.js
+++ jquery.ctabs.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery Tabs Plugin v0.1
+ * jQuery Chrome Tabs Plugin v0.1
  * https://github.com/riga/jquery.ctabs
  *
  * Copyright 2012, Marcel Rieger
@@ -11,8 +11,143 @@
 
 (function($) {
 
-    $.widget('custom.ctabs', $.ui.tabs, {
-        
+    $.widget("custom.ctabs", {
+        version: "0.1",
+        options: {
+            height: "100%",
+            width: "100%",
+            headLeft: "",
+            headLeftWidth: 0,
+            headRight: "",
+            headRightWidth: 0,
+        },
+
+        _create: function() {
+            this._collectSource();
+            this._hideSource();
+            this._createTarget();
+            this._fillTarget();
+        },
+
+        _init: function() {
+            this._initTarget();
+        },
+
+        _collectSource: function() {
+            var that = this;
+            this.tabList = this.element.find("ol, ul").eq(0);
+            this.tabs = this.tabList.children();
+            this.anchors = this.tabs.map(function() {
+                return $("a", this);
+            });
+            this.panels = $();
+            this.store = {};
+            $.each(this.anchors, function(i, anchor) {
+                var hash = anchor.get(0).hash;
+                var panel = that.element.children(hash).eq(0);
+                that.panels = that.panels.add(panel);
+                that.store[hash] = {
+                    hash: hash,
+                    tab: that.tabs.eq(i),
+                    anchor: anchor,
+                    panel: panel,
+                    ctab: null
+                };
+            });
+        },
+
+        _hideSource: function() {
+            this.tabList.hide();
+            this.panels.hide();
+        },
+
+        _createTarget: function() {
+            var that = this;
+            this.element
+                .addClass("ctabs-element")
+                .css({
+                    height: this.options.height,
+                    width: this.options.width
+                });
+            this.nodes = {};
+            this.nodes.head = $("<div>")
+                .addClass("ctabs-head")
+                .appendTo(this.element);
+            this.nodes.body = $("<div>")
+                .addClass("ctabs-body")
+                .appendTo(this.element);
+            this.nodes.headLeft = $("<div>")
+                .addClass("ctabs-head-left ctabs-table-outer")
+                .appendTo(this.nodes.head);
+            $("<div>")
+                .addClass("ctabs-table-inner")
+                .append(this.options.headLeft)
+                .appendTo(this.nodes.headLeft);
+            this.nodes.headRight = $("<div>")
+                .addClass("ctabs-head-right ctabs-table-outer")
+                .appendTo(this.nodes.head);
+            $("<div>")
+                .addClass("ctabs-table-inner")
+                .append(this.options.headRight)
+                .appendTo(this.nodes.headRight);
+            this.nodes.headCenter = $("<div>")
+                .addClass("ctabs-head-center")
+                .appendTo(this.nodes.head);
+        },
+
+        _initTarget: function() {
+            this.nodes.headLeft.css("width", this.options.headLeftWidth);
+            this.nodes.headRight.css("width", this.options.headRightWidth);
+            this.nodes.headCenter.css({
+                left: this.options.headLeftWidth,
+                right: this.options.headRightWidth
+            });
+        },
+
+        _fillTarget: function() {
+            var that = this;
+            $.each(this.store, function(hash) {
+                that._createCTab(hash, undefined);
+            });
+        },
+
+        _createCTab: function(hash, data) {
+            if (!this.store[hash]) {
+                this.add(hash, data);
+            }
+            if (!this.store[hash]) {
+                return this;
+            }
+            this.store[hash].tab.hide();
+            this.store[hash].panel.hide().appendTo(this.nodes.body);
+
+            this.nodes.headCenter.append($("<span>").html(this.store[hash].anchor.html()));
+        },
+
+        add: function(hash, data) {
+            $.extend({title: "", content: ""}, data);
+            var tab = $("<li>").appendTo(this.tabList);
+            var anchor = $("<a>")
+                .attr("href", hash)
+                .append(data.title)
+                .appendTo(tab);
+            var panel = data.content;
+            if (typeof(data.content) == "string") {
+                panel = $("<div>").append(data.content);
+            }
+            this.element.append(panel);
+            this.store[hash] = {
+                hash: hash,
+                tab: tab,
+                anchor: anchor,
+                panel: panel,
+                ctab: null
+            };
+        },
+
+        remove: function(hash) {
+            // TODO
+        }
     });
 
 })(jQuery);
